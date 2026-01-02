961c82cbf4f17009f7b95571cba6b2b3d5f10d3b
XIMalik <abdulmalikawesu@gmail.com>
2026-01-02 10:32:14 +0100

    added config for static

--- settings.py
+++ settings.py
@@ -212,3 +212,9 @@ AUTHENTICATION_BACKENDS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
