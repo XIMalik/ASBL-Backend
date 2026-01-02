80354b7cb40e71e8a4c471a99b89a4c01ef17ac9
XIMalik <abdulmalikawesu@gmail.com>
2026-01-02 10:15:58 +0100

    edited settings.py to allow railway

--- settings.py
+++ settings.py
@@ -17,15 +17,18 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
+DEBUG = False
+
+ALLOWED_HOSTS = ["*.railway.app"]
+
+CSRF_TRUSTED_ORIGINS = ["https://*.railway.app"]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-8wvzjpg=o5rs%nkc+%zwnn$pm0z__p@3v)_u(&ak38@g-50b%1'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -51,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
