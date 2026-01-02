d38019096fb47586ad41e69953f9213d738501bc
XIMalik <abdulmalikawesu@gmail.com>
2026-01-02 11:13:45 +0100

    optimized for railway

--- settings.py
+++ settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=True,
+        conn_health_checks=True,
     )
 }
 
