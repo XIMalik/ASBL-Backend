0bb333df285588de771347623efcdf37215135e5
XIMalik <abdulmalikawesu@gmail.com>
2026-01-17 17:19:11 +0100

    fixed render db issue by inputting dj_db_url settings

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 import os 
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -81,15 +82,27 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# comment out when deploying to git for render/railway
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
+# db setup for render/railway deployment
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
